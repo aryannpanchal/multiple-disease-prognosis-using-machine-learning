c381ac3f43a19082d27d9667306605d28fe74875
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-03-16 12:52:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ with st.sidebar:
 if selected == 'Diabetes Prediction':
 
     # page title
-    st.title('Diabetes Prediction using ML')
+    st.title('Multiple Disease Prognosis using Machine Learning')
 
     # getting the input data from the user
     col1, col2, col3 = st.columns(3)
