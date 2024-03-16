c9b78b3f8551526aa25a2cfb66171ad803b6db18
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-03-16 13:00:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,8 +40,7 @@ if selected == 'Diabetes Prediction':
     st.caption('by Aryan Panchal B609')
     st.caption('Jash Panchal B610')
     st.caption('Ashlesha Padvi B606')
-
-  st.text('Guide: Dr. Sharmila Rathod')
+    st.text('Guide: Dr. Sharmila Rathod')
 
 
 
