d56e3a512594a612f3038ae2d2d2b6ab2eab54c8
Aryan Panchal <98644643+aryannpanchal@users.noreply.github.com>
2024-04-18 22:55:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ if selected == 'Diabetes Prediction':
         Glucose = st.text_input('Glucose Level (mg/dl)')
         
 if len(Glucose) >  2:
-    st.write("Please fix")
+    st.error("Please fix")
 else:
     st.write("Glucose value accepted")
     # code for Prediction    
